28524c8acffaea028acd374a1ee6ecb6aa64ba2e
KSG <hammersang@gmail.com>
2025-04-20 00:58:41 +0900

    pipe study

--- src/app/shared/shared-imports.ts
+++ src/app/shared/shared-imports.ts
@@ -2,11 +2,13 @@ import { NgIf, NgForOf } from '@angular/common';
 import { AsyncPipe } from '@angular/common';
 import { DatePipe } from '@angular/common';
 import {FormsModule} from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 export const sharedImports = [
   NgIf,
   NgForOf,
   AsyncPipe,
   DatePipe,
-  FormsModule
+  FormsModule,
+  CommonModule
 ];
