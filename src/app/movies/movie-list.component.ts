39a8c36739fac61c7b00a72989b71a4643913d6a
sangsahh <hammersang@gmail.com>
2025-04-21 16:34:59 +0900

    fliter study

--- src/app/movies/movie-list.component.ts
+++ src/app/movies/movie-list.component.ts
@@ -1,11 +1,13 @@
 import { Component , OnInit } from "@angular/core";
 import { sharedImports } from '../shared/shared-imports';
 import { movie } from "./movie.model";
+//component 생성
 @Component({
     selector: 'app-movies',
     standalone: true,
     imports: [sharedImports],
     templateUrl: './movie-list.component.html',
+    //스타일 캡슐화화
     styleUrls: ['./movie-list.component.scss']
 })
 export class MovieListComponent implements OnInit {
@@ -13,7 +15,16 @@ export class MovieListComponent implements OnInit {
     imgWidth: number = 55;
     imgMargin: number = 2;
     isImgDisplayed: boolean = false;
-    filterText = 'martrix4';
+    // get,set 만들기기
+    private _filterText = '';
+    get filterText() : string {
+        return this._filterText;
+    }
+    public set filterText(v : string) {
+        this._filterText = v;
+        this.filteredMovies = this.performFilter(v);
+    }
+    
     movies: movie[] = [
         {
             "movieId": 1,
@@ -36,10 +47,23 @@ export class MovieListComponent implements OnInit {
             "imageUrl": "images/spider.png",
         }
     ];
+    //필터 처리
+    filteredMovies: movie[] = this.movies;
+    // 이벤트 생성성
     public toggleImg(): void {
         this.isImgDisplayed = !this.isImgDisplayed;
     }
+    //OnInit 라이프사이클클 체크
     public ngOnInit(): void {
         console.log('앵귤러 라이프사이클: ngOnInit()');
+    }
+    // filterBy를 매개변수, return값 movie[]
+    public performFilter(filterBy: string ): movie[] {
+        //비교하기 전에 소문자로 만들어서 비교
+        filterBy = filterBy.toLowerCase();
+        //영화의 이름을 가져와서 이름을 소문자로 만든후 filterBy와 맞는것이 있다면 리턴함
+        return this.movies.filter((movie: movie) => {
+            return movie.name.toLocaleLowerCase().includes(filterBy);
+        })
     }
 }
